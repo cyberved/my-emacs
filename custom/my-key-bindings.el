c7cc018a7db7b46f7e8f19d83d84ae2b8102de39
cyberved <cyberved@gmail.com>
2014-02-21 00:09:28 +0800

    Enable windmove-default-keybindings if available

--- custom/my-key-bindings.el
+++ custom/my-key-bindings.el
@@ -3,5 +3,8 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
 
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
+
 ;;; my-key-bindings.el ends here
 
