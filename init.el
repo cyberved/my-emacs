f408fdb548444862976368c1556c6962840ec79a
cyberved <cyberved@gmail.com>
2014-02-21 20:24:37 +0800

    Add git-modes and magit directories to load-path

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path "~/.emacs.d/custom/")
 (add-to-list 'load-path "~/.emacs.d/vendor/")
 (let ((default-directory "~/.emacs.d/vendor/"))
-  (normal-top-level-add-to-load-path '("pyenv.el")))
+  (normal-top-level-add-to-load-path '("pyenv.el" "git-modes" "magit")))
 
 (load "my-globals")
 (load "my-modes")
