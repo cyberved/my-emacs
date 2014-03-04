c4629fafb11c8d19febce1082f166772f720a58f
cyberved <cyberved@gmail.com>
2014-03-04 22:02:22 +0800

    Add some additional ibuffer options

--- custom/my-buffers.el
+++ custom/my-buffers.el
@@ -1,5 +1,8 @@
 ;;; my-buffers.el --- Configure Emacs Buffers
 ;;; Code:
+(setq ibuffer-expert t)
+(setq ibuffer-show-empty-filter-groups nil)
+
 (setq ibuffer-saved-filter-groups
       '(("default"
 	 ("dired" (mode . dired-mode))
@@ -9,6 +12,7 @@
 
 (add-hook 'ibuffer-mode-hook
 	  '(lambda ()
+	     (ibuffer-auto-mode 1)
 	     (ibuffer-switch-to-saved-filter-groups "default")))
 
 ;;; my-buffers.el ends here
